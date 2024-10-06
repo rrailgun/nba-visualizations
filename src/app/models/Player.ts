e99bfbe3429b5ad1199ba213b7902f88549ab446
rrailgun <rrailgun456@gmail.com>
2024-10-06 00:17:26 -0400

    create player model

---
+++ src/app/models/Player.ts
@@ -0,0 +1,7 @@
+export default class Player {
+    full_name?: string
+    id?: number;
+    is_active?: boolean;
+    last_name?: string;
+    first_name?: string;
+}
--- src/app/services/player-data.service.ts
+++ src/app/services/player-data.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import Player from '../models/Player';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +10,10 @@ export class PlayerDataService {
   baseURL = '/api'
 
   constructor(private http: HttpClient) {}
+
+  public getPlayers(): Observable<Player[]> {
+    return this.http.get<Player[]>(`${this.baseURL}/players`);
+  }
 
   public getShotData(playerId, season) {
     return this.http.get(`${this.baseURL}/shotchartdetail?PlayerID=${playerId}&Season=${season}`);
