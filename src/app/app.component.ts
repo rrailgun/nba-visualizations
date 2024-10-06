2507517664463f3f4f4a84d0859b1e30e293daa1
rrailgun <rrailgun456@gmail.com>
2024-10-06 00:40:17 -0400

    switch to angular material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ShotChartComponent } from "./charts/shot-chart/shot-chart.component";
 import { PlayerPageComponent } from "./pages/player-page/player-page.component";
-import { NbLayoutModule, NbThemeModule } from '@nebular/theme';
+import { MatSidenavModule } from "@angular/material/sidenav"
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ShotChartComponent, PlayerPageComponent, NbLayoutModule],
+  imports: [RouterOutlet, ShotChartComponent, PlayerPageComponent, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,11 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { NbThemeModule } from '@nebular/theme';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(),
-    importProvidersFrom(NbThemeModule.forRoot())
+    provideHttpClient(), provideAnimationsAsync(),
   ]
 };
